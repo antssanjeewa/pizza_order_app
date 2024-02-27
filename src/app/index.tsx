46c12fb80fd162d5a5b9de685d6d61ddae29790a
Sameera Sanjeewa <sameera.sanjeewa@elegantmedia.lk>
2024-02-27 19:11:43 +0530

    add: auth screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,34 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
 
 const index = () => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-            <Link href={'/(user)'} asChild>
-                <Button title="User" />
+            <Link replace style={styles.button} href={'/(user)'}>
+                <Text>User</Text>
             </Link>
-            <Link href={'/(admin)'} asChild>
-                <Button title="Admin" />
+
+            <Link replace style={styles.button} href={'/(admin)'}>
+                <Text>Admin</Text>
+            </Link>
+
+            <Link replace style={styles.button} href={'/sign-in'}>
+                <Text>Auth</Text>
             </Link>
         </View>
     );
 };
 
 export default index;
+
+const styles = StyleSheet.create({
+    button: {
+        marginBottom: 20,
+        padding: 20,
+        borderRadius: 50,
+        textAlign: 'center',
+        backgroundColor: 'blue',
+        color: 'white'
+    }
+})
