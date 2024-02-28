3fa4b966065b3e1c945a8d5c11847fc2b1361711
Sameera Sanjeewa <sameera.sanjeewa@elegantmedia.lk>
2024-02-28 10:26:16 +0530

    add: supabase

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,12 +1,25 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
 import React, { useState } from 'react';
 import Button from '../../components/Button';
 import Colors from '../../constants/Colors';
-import { Link, Stack } from 'expo-router';
+import { Link, Stack, router } from 'expo-router';
+import { supabase } from '@/lib/supabase';
 
 const SignInScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function signInWithEmail() {
+        setLoading(true);
+        const { error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+
+        if (error) Alert.alert(error.message);
+        setLoading(false);
+    }
 
     return (
         <View style={styles.container}>
@@ -29,7 +42,9 @@ const SignInScreen = () => {
                 secureTextEntry
             />
 
-            <Button text="Sign in" />
+
+            <Button onPress={signInWithEmail} text="Login" />
+
             <Link href="/sign-up" style={styles.textButton}>
                 Create an account
             </Link>
---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+    getItem: (key: string) => {
+        return SecureStore.getItemAsync(key);
+    },
+    setItem: (key: string, value: string) => {
+        SecureStore.setItemAsync(key, value);
+    },
+    removeItem: (key: string) => {
+        SecureStore.deleteItemAsync(key);
+    },
+};
+
+const supabaseUrl = 'https://jmzmmnessrvvuphuhvmp.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptem1tbmVzc3J2dnVwaHVodm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwOTQyNDMsImV4cCI6MjAyNDY3MDI0M30.QWxcAF6v75gZMULpN0DoIYiS_8hdV41M-IuBzNfduBs';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: ExpoSecureStoreAdapter as any,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
